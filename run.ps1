f87edda0059c131c83c5560878f95ef8656aa053
dimakomplekt <dkriskovets@gmail.com>
2026-01-18 03:31:19 +0300

    изменение run.ps1

--- run.ps1
+++ run.ps1
@@ -1,8 +1,25 @@
+# Добавляем OpenCV в PATH
 $env:PATH += ";C:\opencv\build\x64\vc16\bin"
 
-cmake --build build --config Release
+# Если папки build нет — создаём и конфигурируем проект
+if (-not (Test-Path "build")) {
+    Write-Host "build отсуствует  — создаем и запускаем сборку."
+    mkdir build
+    cd build
+    cmake ..
+}
+else {
+    cd build
+}
 
+# Собираем проект
+cmake --build . --config Release
 
+# Если сборка успешна — запускаем программу
 if ($LASTEXITCODE -eq 0) {
-    .\build\Release\OpenCV.exe
+    Write-Host "Запуск программы!"
+    .\Release\OpenCV.exe
+}
+else {
+    Write-Host "Ошибка сборки!"
 }
